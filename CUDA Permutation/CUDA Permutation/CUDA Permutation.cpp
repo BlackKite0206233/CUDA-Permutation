bf6432cf7278cd20a94aec26424f4b6a2a7e06e7
BlackKite0206233 <samuel991640@gmail.com>
2017-07-15 01:53:17 +0800

    remove CUDA

--- CUDA Permutation/CUDA Permutation/CUDA Permutation.cpp
+++ CUDA Permutation/CUDA Permutation/CUDA Permutation.cpp
@@ -1,20 +1,17 @@
-#pragma once
-#include "cuda_runtime.h"
-#include "device_launch_parameters.h"
-#include <curand.h>
-#include <curand_kernel.h>
 #include <cstdio>
 #include <ctime>
 #include <cmath>
 #include <string>
-#include<iostream>
+#include <iostream>
+#include <time.h>
+#include <random>
 
 #include "InputCSV.h"
 #include "OutputCSV.h"
 
 
 using namespace std;
-
+/*
 __device__ bool Compare(int* set, int* winningSet, int size) {
     int Any = 0;
     for (int i = 0; i < size; i++) {
@@ -38,12 +35,7 @@ __device__ bool Compare(int* set, int* winningSet, int size) {
     return true;
 }
 
-// è¨­å®šæ¯å€‹kernelçš„äº‚æ•¸ç¨®å­
-__global__ void SetupCurand(curandState *state, unsigned long seed) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    curand_init(seed, idx, 0, &state[idx]);
-}
-// è·‘æ¨¡æ“¬
+// ¶]¼ÒÀÀ
 __global__ void Simulate(curandState *states, const int colunmSize, const int rowSize, int* reelSets, const int reelSetSize, int* payTable, int winningSetSize, size_t runTimes, size_t* hitTimes, size_t* noHitTimes, const size_t NUM_OF_THREAD) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     curandState localState = states[idx];
@@ -64,29 +56,46 @@ __global__ void Simulate(curandState *states, const int colunmSize, const int ro
                 hitTimes[idx + n * NUM_OF_THREAD] += 1;
             }
         }
-        // ç´€éŒ„no hit.
+        // ¬ö¿ýno hit.
         if (!hit) { noHitTimes[idx] += 1; }
         states[idx] = localState;
     }
     free(set);
 };
 
+*/
+
+void Simulate(const int colunmSize, const int rowSize, int* reelSets, const int reelSetSize, int* payTable, int winningSetSize, size_t runTimes, size_t* hitTimes, size_t* noHitTimes, const size_t NUM_OF_THREAD) {
+    int* set;
+    set = new int[colunmSize * rowSize];
+    for(int t = 0; t < runTimes; t++) {
+        for (int col = 0; col < colunmSize; col++) {
+            unsigned int random = rand() % reelSetSize;
+            for (int row = 0; row < rowSize; row++) {
+                set[row + col * rowSize] = reelSets[(random + row) % reelSetSize];
+            }
+        }
+    }
+}
+
 int main(int argc, char** argv) {
+    srand(time(NULL));
+
     const unsigned int RUN_TIMES = 50000000;
 
-    // åŠ å…¥ Console åƒæ•¸
+    // ¥[¤J Console °Ñ¼Æ
     if (argc != 3) { printf(".exe [input file] [output file]\n"); return 1; }
     string intputPath = argv[1];
     string outputPath = argv[2];
 
-    // è¨­å®šè¼¸å…¥è¼¸å‡ºæª”æ¡ˆ
+    // ³]©w¿é¤J¿é¥XÀÉ®×
     InputCSV inputFile(intputPath);
     OutputCSV outputFile(outputPath);
 
-    // è¨ˆæ™‚é–‹å§‹
+    // ­p®É¶}©l
     unsigned long cStart = clock();
 
-    // æ¨¡æ“¬ Column*Row çš„ç›¤é¢.
+    // ¼ÒÀÀ Column*Row ªº½L­±.
     const int COLUMN_SIZE = inputFile.getPermutationColumnSize();
     const int REEL_ROW_SIZE = inputFile.getReelRowSize();
 
@@ -94,7 +103,7 @@ int main(int argc, char** argv) {
     const string *SYMBOLS = inputFile.getPermutationElements();
     const int SYMBOLS_SIZE = inputFile.getPermutationElementsCount();
 
-    // Stops.(åœæ­¢é»ž = Reelä¸Šçš„å…ƒç´ )
+    // Stops.(°±¤îÂI = Reel¤Wªº¤¸¯À)
     const int* STOPS = inputFile.getReelSet();
     const int STOPS_SIZE = inputFile.getReelSetSize();
 
@@ -103,68 +112,44 @@ int main(int argc, char** argv) {
     // Size of pay table(Element count).
     const int PAY_TABLE_SIZE = inputFile.getPayTableSize();
 
-    // PAY_TABLE è£¡è®Šæ•¸çš„æ•¸é‡ = sizeof(PAY_TABLE) / sizeof(int)
+    // PAY_TABLE ¸ÌÅÜ¼Æªº¼Æ¶q = sizeof(PAY_TABLE) / sizeof(int)
     const int PAY_TABLE_REAL_SIZE = PAY_TABLE_SIZE * COLUMN_SIZE * REEL_ROW_SIZE;
 
-    //---------------------Begin of cuda-----------------------------
+    //---------------------Begin of test-----------------------------
     size_t *hitTimes;
     size_t *host_hitTimes;
-    size_t *dev_hitTimes;
 
     size_t *host_noHitTimes;
-    size_t *dev_noHitTimes;
 
 
-    int* dev_reelSets;
-    int* dev_winningSets;
+    int* reelSets;
+    int* winningSets;
 
 
-    // è¨­å®š thread & block.
+    // ³]©w thread & block.
     size_t threads = 100;
     size_t blocks = 1000;
 
     size_t NumOfThread = blocks * threads, kernelRunTimes = ceil(RUN_TIMES / NumOfThread);
     printf("Total times: %d\nBlock count: %d\nThread count: %d\nKernelRunTimes: %d\n", RUN_TIMES, blocks, threads, kernelRunTimes);
 
-    // é…ç½®Host memory.
-    hitTimes = (size_t*) malloc(PAY_TABLE_SIZE * sizeof(size_t));
-    host_hitTimes = (size_t*) malloc(NumOfThread * PAY_TABLE_SIZE * sizeof(size_t));
-    host_noHitTimes = (size_t*) malloc(NumOfThread * sizeof(size_t));
+    // set memory.
+    hitTimes = new size_t[PAY_TABLE_SIZE];
+    host_hitTimes =  new size_t[NumOfThread * PAY_TABLE_SIZE];
+    host_noHitTimes = new size_t[NumOfThread];
 
+    reelSets = new int[STOPS_SIZE];
+    memcpy(reelSets, STOPS, STOPS_SIZE);
 
-    // é…ç½®Device memory.
-    cudaMalloc((void**) &dev_hitTimes, NumOfThread * PAY_TABLE_SIZE * sizeof(size_t));
-    cudaMalloc((void**) &dev_noHitTimes, NumOfThread * sizeof(size_t));
-
-
-    // Declare reel sets.
-    cudaMalloc((void**) &dev_reelSets, STOPS_SIZE * sizeof(int));
-    cudaMemcpy(dev_reelSets, STOPS, STOPS_SIZE * sizeof(int), cudaMemcpyHostToDevice);
-
-    // Declare pay table.
-    cudaMalloc((void**) &dev_winningSets, PAY_TABLE_REAL_SIZE * sizeof(int));
-    cudaMemcpy(dev_winningSets, PAY_TABLE, PAY_TABLE_REAL_SIZE * sizeof(int), cudaMemcpyHostToDevice);
-
-    // Setup random seed for each threads.
-    curandState* devStates;
-    cudaMalloc(&devStates, NumOfThread * sizeof(curandState));
-    SetupCurand <<<blocks, threads >>> (devStates, time(NULL));
+    winningSets = new int[PAY_TABLE_REAL_SIZE];
+    memcpy(winningSets, PAY_TABLE, PAY_TABLE_REAL_SIZE);
 
     // Simulate.
-    Simulate <<<blocks, threads >>> (devStates, COLUMN_SIZE, REEL_ROW_SIZE, dev_reelSets, STOPS_SIZE, dev_winningSets, PAY_TABLE_SIZE, kernelRunTimes, dev_hitTimes, dev_noHitTimes, NumOfThread);
-
-    // Copy device memory to host.
-    cudaMemcpy(host_hitTimes, dev_hitTimes, NumOfThread * PAY_TABLE_SIZE * sizeof(size_t), cudaMemcpyDeviceToHost);
-    cudaMemcpy(host_noHitTimes, dev_noHitTimes, NumOfThread * sizeof(size_t), cudaMemcpyDeviceToHost);
+    Simulate(COLUMN_SIZE, REEL_ROW_SIZE, reelSets, STOPS_SIZE, winningSets, PAY_TABLE_SIZE, kernelRunTimes, host_hitTimes, host_noHitTimes, NumOfThread);
 
 
-    //é‡‹æ”¾Memory.
-    cudaFree(dev_reelSets);
-    cudaFree(dev_winningSets);
-    cudaFree(dev_hitTimes);
-
-    //---------------------End of cuda-----------------------------
-    // ç®— Hit.
+    //---------------------End of test-----------------------------
+    // ºâ Hit.
     size_t totalHitTimes = 0;
     for (size_t i = 0; i < PAY_TABLE_SIZE; i++) {
         hitTimes[i] = 0;
@@ -173,21 +158,21 @@ int main(int argc, char** argv) {
         }
         totalHitTimes += hitTimes[i];
     }
-    // ç®— No Hit.
+    // ºâ No Hit.
     size_t noHitTimes = 0;
     for (size_t t = 0; t < NumOfThread; t++) {
         noHitTimes += host_noHitTimes[t];
     }
-    // è¨ˆæ™‚å®Œäº†
+    // ­p®É§¹¤F
     unsigned long spendTime = clock() - cStart;
 
     // Console print.
     printf("CUDA run %lu ms.\n", spendTime);
     printf("Output to %s... \n", outputPath.c_str());
 
-    // è¼¸å‡º
+    // ¿é¥X
     outputFile.WriteTitle(blocks, threads, RUN_TIMES, spendTime, STOPS_SIZE, COLUMN_SIZE, REEL_ROW_SIZE, totalHitTimes, (double)totalHitTimes / RUN_TIMES);
-    
+
     // Output No hit frequency.
     outputFile.WriteHitFreq("No Hit", noHitTimes, (double) noHitTimes / RUN_TIMES);
 
