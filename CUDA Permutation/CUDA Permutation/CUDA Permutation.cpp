e83b0e6c47ec9ea11ead6b7ee789d34af942e467
BlackKite0206233 <samuel991640@gmail.com>
2017-07-16 01:26:34 +0800

    simulate without CUDA

--- CUDA Permutation/CUDA Permutation/CUDA Permutation.cpp
+++ CUDA Permutation/CUDA Permutation/CUDA Permutation.cpp
@@ -11,61 +11,28 @@
 
 
 using namespace std;
-/*
-__device__ bool Compare(int* set, int* winningSet, int size) {
+
+bool Compare(int* set, int* winningSet, int size) {
     int Any = 0;
     for (int i = 0; i < size; i++) {
         if (set[i] != -1 && winningSet[i] > 0) {
             // ordinary compare
-            if (set[i] != winningSet[i]) {
+            if (set[i] != winningSet[i]) 
                 return false;
-            }
         } else if (set[i] != -1 && winningSet[i] == -1) {
             // any
-            if (Any == 0) {
+            if (Any == 0) 
                 Any = set[i];
-            } else {
-                if (set[i] != Any) {
+            else 
+                if (set[i] != Any)
                     return false;
-                }
-            }
         }
     }
 
     return true;
 }
 
-// 跑模擬
-__global__ void Simulate(curandState *states, const int colunmSize, const int rowSize, int* reelSets, const int reelSetSize, int* payTable, int winningSetSize, size_t runTimes, size_t* hitTimes, size_t* noHitTimes, const size_t NUM_OF_THREAD) {
-    int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    curandState localState = states[idx];
-    int* set;
-    set = (int*) malloc(colunmSize*rowSize * sizeof(int));
-    for (int t = 0; t < runTimes; t++) {
-        for (int col = 0; col < colunmSize; col++) {
-            unsigned int rand = curand(&localState) % reelSetSize;
-            for (int row = 0; row < rowSize; row++) {
-                set[row + col * rowSize] = reelSets[(rand + row) % reelSetSize];
-            }
-        }
-        bool hit = false;
-        for (int n = 0; n < winningSetSize; n++) {
-            if (Compare(set, (payTable + colunmSize * rowSize * n), colunmSize * rowSize)) {
-                hit = true;
-                // Hit.
-                hitTimes[idx + n * NUM_OF_THREAD] += 1;
-            }
-        }
-        // 紀錄no hit.
-        if (!hit) { noHitTimes[idx] += 1; }
-        states[idx] = localState;
-    }
-    free(set);
-};
-
-*/
-
-void Simulate(const int colunmSize, const int rowSize, int* reelSets, const int reelSetSize, int* payTable, int winningSetSize, size_t runTimes, size_t* hitTimes, size_t* noHitTimes, const size_t NUM_OF_THREAD) {
+void Simulate(const int colunmSize, const int rowSize, int* reelSets, const int reelSetSize, int* payTable, int winningSetSize, size_t runTimes, size_t* hitTimes, size_t* noHitTimes) {
     int* set;
     set = new int[colunmSize * rowSize];
     for(int t = 0; t < runTimes; t++) {
@@ -75,6 +42,17 @@ void Simulate(const int colunmSize, const int rowSize, int* reelSets, const int 
                 set[row + col * rowSize] = reelSets[(random + row) % reelSetSize];
             }
         }
+
+        bool hit = false;
+        for (int n = 0; n < winningSetSize; n++) {
+            if (Compare(set, (payTable + colunmSize * rowSize * n), colunmSize * rowSize)) {
+                hit = true;
+                hitTimes[n]++;
+            }
+        }
+        // 紀錄no hit.
+        if (!hit) 
+            (*noHitTimes)++; 
     }
 }
 
@@ -125,18 +103,22 @@ int main(int argc, char** argv) {
     int* reelSets;
     int* winningSets;
 
-
+    
     // 設定 thread & block.
     size_t threads = 100;
     size_t blocks = 1000;
 
     size_t NumOfThread = blocks * threads, kernelRunTimes = ceil(RUN_TIMES / NumOfThread);
     printf("Total times: %d\nBlock count: %d\nThread count: %d\nKernelRunTimes: %d\n", RUN_TIMES, blocks, threads, kernelRunTimes);
+    
 
     // set memory.
-    hitTimes = new size_t[PAY_TABLE_SIZE];
-    host_hitTimes =  new size_t[NumOfThread * PAY_TABLE_SIZE];
-    host_noHitTimes = new size_t[NumOfThread];
+    host_hitTimes =  new size_t[PAY_TABLE_SIZE];
+    host_noHitTimes = new size_t;
+
+    for(int i = 0; i < PAY_TABLE_SIZE; i++)
+        host_hitTimes[i] = 0;
+    *host_noHitTimes = 0;
 
     reelSets = new int[STOPS_SIZE];
     memcpy(reelSets, STOPS, STOPS_SIZE);
@@ -145,24 +127,18 @@ int main(int argc, char** argv) {
     memcpy(winningSets, PAY_TABLE, PAY_TABLE_REAL_SIZE);
 
     // Simulate.
-    Simulate(COLUMN_SIZE, REEL_ROW_SIZE, reelSets, STOPS_SIZE, winningSets, PAY_TABLE_SIZE, kernelRunTimes, host_hitTimes, host_noHitTimes, NumOfThread);
+    Simulate(COLUMN_SIZE, REEL_ROW_SIZE, reelSets, STOPS_SIZE, winningSets, PAY_TABLE_SIZE, 50000000, host_hitTimes, host_noHitTimes);
 
 
     //---------------------End of test-----------------------------
     // 算 Hit.
     size_t totalHitTimes = 0;
     for (size_t i = 0; i < PAY_TABLE_SIZE; i++) {
-        hitTimes[i] = 0;
-        for (size_t t = 0; t < NumOfThread; t++) {
-            hitTimes[i] += host_hitTimes[i * NumOfThread + t];
-        }
-        totalHitTimes += hitTimes[i];
+        totalHitTimes += host_hitTimes[i];
     }
-    // 算 No Hit.
-    size_t noHitTimes = 0;
-    for (size_t t = 0; t < NumOfThread; t++) {
-        noHitTimes += host_noHitTimes[t];
-    }
+
+    cout << totalHitTimes << endl;
+
     // 計時完了
     unsigned long spendTime = clock() - cStart;
 
@@ -174,17 +150,19 @@ int main(int argc, char** argv) {
     outputFile.WriteTitle(blocks, threads, RUN_TIMES, spendTime, STOPS_SIZE, COLUMN_SIZE, REEL_ROW_SIZE, totalHitTimes, (double)totalHitTimes / RUN_TIMES);
 
     // Output No hit frequency.
-    outputFile.WriteHitFreq("No Hit", noHitTimes, (double) noHitTimes / RUN_TIMES);
+    outputFile.WriteHitFreq("No Hit", *host_noHitTimes, (double)*host_noHitTimes / RUN_TIMES);
 
     // Output hit frequency.
     for (int i = 0; i < PAY_TABLE_SIZE; i++) {
-        outputFile.WriteHitFreq(inputFile.getPayTableFileName(i), hitTimes[i], (double) hitTimes[i] / RUN_TIMES);
+        outputFile.WriteHitFreq(inputFile.getPayTableFileName(i), host_hitTimes[i], (double) host_hitTimes[i] / RUN_TIMES);
     }
 
 
 
     outputFile.Close();
-    delete[] hitTimes;
+    delete[] host_hitTimes;
+    delete[] reelSets;
+    delete[] winningSets;
     printf("Finish.\n");
     return 0;
 }
